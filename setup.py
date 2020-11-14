04cb77e12c41f488734e67377957d8de890aef1f
Hugo Proença <hugo.manuel.proenca@gmail.com>
2020-11-14 15:49:34 +0000

    corrected setup.py file

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ with open('README.md') as f:
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-
 setuptools.setup(
     name='rulelist',
-    version='0.0.1',
+    version='0.0.2',
     description='Learn rule lists from data for classification, regression or subgroup discovery',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires= INSTALL_REQUIRES,
     url='https://github.com/HMProenca/RuleList',
     license='MIT License',
     author='Hugo Proenca',
