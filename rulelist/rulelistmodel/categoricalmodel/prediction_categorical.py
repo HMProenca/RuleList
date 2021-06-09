e76eba8a1f82c4ccfd563e3aee00f4e3e9a70fce
Hugo Proença <hugo.manuel.proenca@gmail.com>
2021-06-09 14:42:51 +0200

    Added laplace smoothing for the calculation of categorical probabilities.

--- rulelist/rulelistmodel/categoricalmodel/prediction_categorical.py
+++ rulelist/rulelistmodel/categoricalmodel/prediction_categorical.py
@@ -6,6 +6,18 @@ def point_value_categorical(statistics):
     return class_labels
 
 def probability_categorical(statistics,target):
+    """ Computes the probability with laplace smoothing.
+
+    Adds a little pseudocount (epsilon) which makes for a more balanced probability.
+    An epsilon of 0.5 is the Jeffrey's prior for Dirichlet's distribution, and an epsilon of 1 is the uniform prior.
+
+    :param statistics:
+    :param target:
+    :return:
+    """
     usage = statistics.usage
-    probabilities = np.array([usg_cl/usage for usg_cl in statistics.usage_per_class[target].values()])
+    n_classes = statistics.number_classes[target]
+    epsilon = 0.5
+    probabilities = np.array([(usg_cl+epsilon)/(usage+epsilon*n_classes)
+                              for usg_cl in statistics.usage_per_class[target].values()])
     return probabilities
